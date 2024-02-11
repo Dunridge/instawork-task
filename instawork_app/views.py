48357147bdbd72c25cf17097361af8e0f2660b6a
Dunridge <Polinka7max@gmail.com>
2024-02-10 21:16:47 -0800

    add-page. pass stored members

--- instawork_app/views.py
+++ instawork_app/views.py
@@ -35,14 +35,35 @@ def index(request):
         }
     ]
 
+    session_members = request.session.get('members', [])
+    all_members = members + session_members
+    print('session_members [index]', len(session_members))
+    print('members [index]', len(members))
+    print('all_members [index]', len(all_members))
+
     context = {
-        'members': members,
+        'members': all_members,
     }
 
     return render(request, 'instawork_app/index.html', context)
 
 def add(request): 
     context = {}
+    # you need to trigger this from the template
+    if request.method == 'POST': 
+        new_member = {
+            'name': 'Max',
+            'position': 'Software engineer',
+            'phone': '111 111 1111',
+            'email': 'max@gmail.com',
+        }
+        members = request.session.get('members', [])
+        print('members [add]:', len(members))
+
+        members.append(new_member)
+        request.session['members'] = members
+    
+    
     return render(request, 'add_member/index.html', context)
 
 def edit(request): 
