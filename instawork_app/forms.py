54844fbda5e20e3fd039f7b9478bdfb0de8fe121
Dunridge <Polinka7max@gmail.com>
2024-02-11 20:28:12 -0800

    add-role. add radio select

---
+++ instawork_app/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class MemberForm(forms.Form):
+    name = forms.CharField(max_length=255)
+    surname = forms.CharField(max_length=255)
+    phone = forms.CharField(max_length=20)
+    email = forms.EmailField()
+    role = forms.ChoiceField(
+        choices=[('regular', 'Regular - Can\'t delete members'), ('admin', 'Admin - Can delete members')],
+        widget=forms.RadioSelect
+    )
